62067ef2b59b970d354ac90482eb6619523316f1
kronos <kronos@192.168.0.7>
2017-04-12 02:55:04 +0200

    test

--- locales/available.py
+++ locales/available.py
@@ -17,7 +17,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-
 """Defines a dict of all available locales to the language name"""
 
 OFFSET = 127462 - ord('A')
